389403e4936ebff742c202f6acd096d053ed0bcc
isadam12 <isabelladambrosio12@gmail.com>
2022-03-11 13:51:41 -0500

    first commit

---
+++ proyecto cafeteria 1 1 y 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Bienvenido a la cafeteria C" << endl;
+	
+	cout << "Para entrar a la cafeteria oprima el boton intro" << endl;
+	cin.get()
+    ;int puesto=1;
+    cout<<"Tu numero de mesa es "<<puesto;
+    cout<<endl;
+    return 0;
+}
