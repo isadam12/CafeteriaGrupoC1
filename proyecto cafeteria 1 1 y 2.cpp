43af78389676b0f59942519a4de6b34d3db645c8
nelson507pty <angelkreew@gmail.com>
2022-03-12 15:10:42 -0500

    Actualizado

--- proyecto cafeteria 1 1 y 2.cpp
+++ proyecto cafeteria 1 1 y 2.cpp
@@ -28,20 +28,104 @@ int hora=0;
 int fin=0;
 	if (opcion == 1) {
 		cout<<"Ha seleccionado Plato del Dia. Estara listo en un momento. "<<endl;
+    while (fin==0){
+	system("cls");
+	cout << "Plato del dia" << endl;
+	cout << hora << ":" << minuto << ":" << segundo << endl;
+	sleep_for(seconds(1));
+	segundo--;
+	
+	if (minuto==0 && segundo==3){
+	fin=1;
+}
+	
+	if (segundo==0) {
+		minuto--;
+		segundo=0;
+	}
+	
+	if (segundo==0){
+		minuto--;
+		segundo=60;
+	}
+}
+
+	system("cls");
+	cout << "0:0:0"<< endl; 
+	cout << "Orden Lista. Pulse cualquier tecla para aceptar la entrega." << endl;
+
+getch();
 
 	}
 	else if (opcion == 2) {
 	cout<<"Ha seleccionado Recomendacion del chef. Estara listo en un momento. "<<endl;
+ while (fin==0){
+	system("cls");
+	cout << "Recomendacion del Chef" << endl;
+	cout << hora << ":" << minuto << ":" << segundo << endl;
+	sleep_for(seconds(1));
+	segundo--;
+	
+	if (minuto==0 && segundo==5){
+	fin=1;
+}
+	
+	if (segundo==0) {
+		minuto--;
+		segundo=0;
+	}
+	
+	if (segundo==0){
+		minuto--;
+		segundo=60;
+	}
+}
+
+	system("cls");
+	cout << "0:0:0"<< endl; 
+	cout << "Orden Lista. Pulse cualquier tecla para aceptar la entrega." << endl;
+
+getch();
 
     }
     else if (opcion == 3) {
     	cout<<"Ha seleccionado Pizza napolitana. Estara listo en un momento. "<<endl;
 
+while (fin==0){
+	system("cls");
+	cout << "Pizza Napolitana" << endl;
+	cout << hora << ":" << minuto << ":" << segundo << endl;
+	sleep_for(seconds(1));
+	segundo--;
+	
+	if (minuto==0 && segundo==2){
+	fin=1;
+}
+	
+	if (segundo==0) {
+		minuto--;
+		segundo=0;
+	}
+	
+	if (segundo==0){
+		minuto--;
+		segundo=60;
+	}
+}
+	system("cls");
+	cout << "0:0:0"<< endl; 
+	cout << "Orden Lista. Pulse cualquier tecla para aceptar la entrega." << endl;
+
+getch();
+	
 	}
 	else {
 		cout<<"----------------------------------------------------------------"<<endl;
 		cout<<"Ha ocurrido un error o el numero no es valido. Intente de nuevo."<<endl;
 		cout<<"----------------------------------------------------------------"<<endl;
 	}
+	    cout<<"--------------------------------------------------------------"<<endl;
+		cout<<"Su orden ha sido entregada con exito. Gracias por preferirnos!"<<endl;
+		cout<<"--------------------------------------------------------------"<<endl;
     return 0;
 }
